2ad29e07dab9fd5b55b2a8c55ef3e1892f53d441
benckx <benoit.vleminckx@centrica.com>
2026-05-07 08:07:02 +0200

    board-gui-example

---
+++ scripts/src/main/kotlin/io/elephantchess/scripts/minification/MinifyDist.kt
@@ -0,0 +1,17 @@
+package io.elephantchess.scripts.minification
+
+import java.io.File
+
+fun main() {
+    val prefix = "webapp/src/main/resources/public/dist/0.0.2/"
+    val files =
+        listOf(
+            "board.css",
+            "xiangqi.js",
+            "board-gui.js"
+        )
+            .map { prefix + it }
+            .map { File(it) }
+
+    minifyIfNeeded(files)
+}
