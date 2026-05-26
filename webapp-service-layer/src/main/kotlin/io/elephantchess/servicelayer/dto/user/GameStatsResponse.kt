8ddc69cfbebd309c4ea5213b29ef5fb5f70f37ce
benckx <benoit.vleminckx@protonmail.com>
2026-05-26 12:00:29 +0200

    refactor LLM code a little

--- webapp-service-layer/src/main/kotlin/io/elephantchess/servicelayer/dto/user/GameStatsResponse.kt
+++ webapp-service-layer/src/main/kotlin/io/elephantchess/servicelayer/dto/user/GameStatsResponse.kt
@@ -1,6 +1,30 @@
 package io.elephantchess.servicelayer.dto.user
 
 data class GameStatsResponse(
-    val ratings: TimeCategoryStatsDto,
-    val pvp: TimeCategoryPlayerVsPlayerStatsDto,
+    val ratings: RatingsPerTimeCategory,
+    val pvp: NumberOfGamePerTimeCategory,
+)
+
+data class RatingsPerTimeCategory(
+    val bullet: Int,
+    val blitz: Int,
+    val rapid: Int,
+    val classical: Int,
+    val severalDays: Int,
+    val correspondence: Int,
+)
+
+data class NumberOfGamePerTimeCategory(
+    val bullet: NumberOfOutcomes,
+    val blitz: NumberOfOutcomes,
+    val rapid: NumberOfOutcomes,
+    val classical: NumberOfOutcomes,
+    val severalDays: NumberOfOutcomes,
+    val correspondence: NumberOfOutcomes,
+)
+
+data class NumberOfOutcomes(
+    val wins: Int,
+    val losses: Int,
+    val draws: Int,
 )
