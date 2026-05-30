2cd1697930a4cc4e5ae73fe8c7a3ce23d9fac9e0
benckx <benoit.vleminckx@protonmail.com>
2026-05-30 10:15:12 +0200

    minor edit

--- webapp/src/main/kotlin/io/elephantchess/webapp/rendering/BoardGuiExampleRenderer.kt
+++ webapp/src/main/kotlin/io/elephantchess/webapp/rendering/BoardGuiExampleRenderer.kt
@@ -1,9 +1,12 @@
 package io.elephantchess.webapp.rendering
 
 import io.elephantchess.htmlrenderer.HtmlRenderer.Companion.CDN_BASE
+import io.elephantchess.htmlrenderer.KtorHtmlBuilderTagResolver
 import io.elephantchess.htmlrenderer.SimpleValueTagResolver
 import io.elephantchess.htmlrenderer.TagResolver
 import io.elephantchess.utils.ResourceUtils.resourceAsString
+import kotlinx.html.link
+import kotlinx.html.script
 
 class BoardGuiExampleRenderer(private val simplePageRenderer: SimplePageRenderer) {
 
@@ -37,22 +40,20 @@ class BoardGuiExampleRenderer(private val simplePageRenderer: SimplePageRenderer
                 .let(::escapeHtml)
         }
 
-    private fun boardGuiAssetsTagResolver(useCdn: Boolean): TagResolver =
-        CallbackTagResolver("board_gui_assets") {
-            var distPath = "/dist/$DIST_VERSION"
-            if (useCdn) {
-                distPath = "${CDN_BASE}$distPath"
-            }
-            """
-            <link rel="stylesheet" href="$distPath/board.min.css"/>
-            <script defer src="$distPath/xiangqi.min.js"></script>
-            <script defer src="$distPath/board-gui.min.js"></script>
-            """.trimIndent()
+    private fun boardGuiAssetsTagResolver(useCdn: Boolean): TagResolver {
+        val distPath = if (useCdn) "${CDN_BASE}/dist/$DIST_VERSION" else "/dist/$DIST_VERSION"
+
+        return KtorHtmlBuilderTagResolver("board_gui_assets") {
+            link(rel = "stylesheet", href = "$distPath/board.min.css")
+            script(src = "$distPath/xiangqi.min.js") { defer = true }
+            script(src = "$distPath/board-gui.min.js") { defer = true }
         }
+    }
 
     companion object {
 
         const val DIST_VERSION = "0.1.1"
 
     }
+
 }
