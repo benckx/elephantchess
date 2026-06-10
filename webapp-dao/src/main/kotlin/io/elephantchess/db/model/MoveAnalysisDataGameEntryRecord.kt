f484b86bd7f8ea8a4423dbd028fc0eb755c8f6b2
benckx <benoit.vleminckx@protonmail.com>
2026-06-10 22:28:33 +0200

    minor edits

--- webapp-dao/src/main/kotlin/io/elephantchess/db/model/MoveAnalysisDataGameEntryRecord.kt
+++ webapp-dao/src/main/kotlin/io/elephantchess/db/model/MoveAnalysisDataGameEntryRecord.kt
@@ -8,5 +8,5 @@ data class MoveAnalysisDataGameEntryRecord(
     val first: Instant,
     val last: Instant,
     val totalAnalyzedMoves: Int,
-    val analyzedFromBatch: Boolean = false,
+    val analyzedFromBatch: Boolean,
 )
