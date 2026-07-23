0ba8b4a3c66b37dde4418618893a3bc6554ae06f
lucaferranti <lucaa.ferranti@gmail.com>
2026-07-23 20:17:34 +0300

    update pikafish version

--- engine-api/src/main/kotlin/io/elephantchess/engines/process/PikafishEngineId.kt
+++ engine-api/src/main/kotlin/io/elephantchess/engines/process/PikafishEngineId.kt
@@ -8,7 +8,16 @@ object PikafishEngineId : EngineId() {
 
     override val displayName: String = "Pikafish"
 
-    override fun pathOfExecutable(version: String?) = "pikafish/$version/pikafish-modern"
+    override fun pathOfExecutable(version: String?): String {
+        val releaseYear = version?.substringBefore("-")?.toIntOrNull()
+        val executableName = if (releaseYear != null && releaseYear >= 2026) {
+            "pikafish-sse41-popcnt"
+        } else {
+            "pikafish-modern"
+        }
+
+        return "pikafish/$version/$executableName"
+    }
 
     override fun makeProcess(
         config: EngineConfig,
