e3fcec4e92ccdd80019bdb01d6c815344fc32bc6
benckx <benoit.vleminckx@protonmail.com>
2026-07-27 09:30:47 +0200

    bump board-gui version

--- webapp/src/main/kotlin/io/elephantchess/webapp/rendering/BoardGuiExampleRenderer.kt
+++ webapp/src/main/kotlin/io/elephantchess/webapp/rendering/BoardGuiExampleRenderer.kt
@@ -53,7 +53,7 @@ class BoardGuiExampleRenderer(private val simplePageRenderer: SimplePageRenderer
 
     companion object {
 
-        const val DIST_VERSION = "0.1.2"
+        const val DIST_VERSION = "0.1.3"
 
     }
 
